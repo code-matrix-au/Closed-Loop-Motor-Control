b9929c29037b1a746090ce3d8e75c8f30971b51b
code-matrix-au <ashwin.bhanderi102@gmail.com>
2020-10-10 00:50:00 +1100

    Refactoring code & tunning PID for Smooth ramping

--- src/main.cpp
+++ src/main.cpp
@@ -1,48 +1,54 @@
 #include <Arduino.h>
-#include "Motor.h"
+#include <Encoder.h>
+#include <Metro.h>
+#include <PID_v1.h>
+#include <Servo.h>
 
-Motor metro = Motor();
+double Setpoint, Input, Output;
 
-/*
-int setPWM(int val)
-{
-  
-  while (PWM != val)
-  {
+double consKp = 0.05, consKi = 0, consKd = 0.05;
 
-    if (timer.check() == 1)
-    {
-      if (state == HIGH)
-      {
-        state = LOW;
-        timer.interval(25);
-        PWM++;
-        return PWM;
-      }
-      else
-      {
-        timer.interval(25);
-        state = HIGH;
-      }
-    }
-    return -999;
-  }
-}
+PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
 
+Servo motor;
 
-*/
+Encoder myEnc(22, 23);
+
+long oldPosition = -999;
+
 void setup()
 {
-  metro.init();
+  motor.attach(13);
+  myPID.SetOutputLimits(0,500);
+  myPID.SetMode(AUTOMATIC);
   Serial.begin(115200);
 }
 
 void loop()
 {
-  // metro.setSpeed(100);
-  
-   metro.getInterupt();
-   metro.setDistance(999);
 
- //  metro.emergencyStop();
+  long newPosition = myEnc.read();
+  if (newPosition != oldPosition)
+  {
+    oldPosition = newPosition;
+    Input = newPosition;
+  }
+
+  myPID.Compute();
+  double gap = (Setpoint - Input);
+  int out;
+  if (gap < 0)
+  {
+    myPID.SetControllerDirection(REVERSE);
+    out = map(Output, 0, 500, 1500, 1000);
+  }
+  else
+  {
+    myPID.SetControllerDirection(DIRECT);
+    out = map(Output, 0, 500, 1500, 2000);
+  }
+  motor.write(out);
+  Serial.print(Input);
+  Serial.print("  ");
+  Serial.println(out);
 }
