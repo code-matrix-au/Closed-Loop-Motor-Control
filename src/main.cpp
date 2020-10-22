6f8f3d08444ec4b61848d883579c58173208558e
code-matrix-au <ashwin.bhanderi102@gmail.com>
2020-10-23 03:50:11 +1100

    Motor direction, speed control & stop is working.

--- src/main.cpp
+++ src/main.cpp
@@ -11,25 +11,40 @@ double consKp = 0.1, consKi = 0.0001, consKd = 0.2;
 PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
 
 Servo motor;
-
 Servo door;
-
 Encoder myEnc(22, 23);
 
-long oldPosition = -999;
+Metro metroStop = Metro(50);
+Metro doorTime = Metro(100);
+Metro print_val = Metro(100);
+int print_state = HIGH;
+struct Value
+{
+  long oldPosition;
+  long distance;
+  int stopState;
+  int doorlock;
+  unsigned int motorOut;
+  bool doorState;
+  bool metro_stopped;
+  int lastState;
+  Metro ramp_up;
+  int ramp_state;
 
-int motorOut = 1500; // value to be writen out to the motor
+} value = {
+    -999,  // oldPosition
+    0,     // distance
+    HIGH,  // stopState
+    LOW,   // doorlock
+    1500,  // motorOut
+    false, // doorState
+    false, // metro_stopped
+    0,
+    50,
+    HIGH}; // lastState
 
-int stopState = HIGH;
-
-Metro metroStop = Metro(0);
-Metro doorTime = Metro(0);
-
-boolean doorState = false;
-
-long disthold;
-
- enum {
+enum State
+{
   STOP,
   START,
   EAST,
@@ -37,22 +52,23 @@ long disthold;
   OPEN,
   CLOSE,
   EMERGENCY
-} metrostate;
+} state;
 
 void Stop()
 {
 
-  if (motorOut != 1500)
+  if (value.motorOut != 1500)
   {
+    value.metro_stopped = false;
     int val;
 
-    if (motorOut > 1500)
+    if (value.motorOut > 1500)
     {
-      val = -1;
+      val = -10;
     }
-    else if (motorOut < 1500)
+    else if (value.motorOut < 1500)
     {
-      val = 1;
+      val = 10;
     }
     else
     {
@@ -61,26 +77,27 @@ void Stop()
 
     if (metroStop.check() == 1) // check if the metro has passed its interval .
     {
-      if (stopState == HIGH)
+      if (value.stopState == HIGH)
       {
-        stopState = LOW;
-        motorOut = +val;
+        value.stopState = LOW;
       }
       else
       {
-        stopState = HIGH;
+        value.stopState = HIGH;
       }
-      motor.write(motorOut);
+      value.motorOut += val;
+      motor.write(value.motorOut);
+      //Serial.println(value.motorOut);
     }
   }
+  value.metro_stopped = true;
 }
 
 void setDistance(long var)
 {
-  if (disthold != var)
+  if (var != Setpoint)
   { // checks if we received new value and updates setpoint.
     Setpoint = var;
-    disthold = var;
   }
   int out;
   double gap = (Setpoint - Input);
@@ -95,37 +112,112 @@ void setDistance(long var)
     out = map(Output, 0, 500, 1500, 1000);
   }
   myPID.Compute();
-  motor.write(out);
+  value.motorOut = out;
+  motor.write(value.motorOut);
 }
 
+void metroDoor(int pos)
+{
 
-void metroDoor(int pos){
-
-  if(pos == 1 && doorState == false){ // close door
-    doorState = true;
-
+  if (pos == 1 && value.doorState == false)
+  {
+    if (doorTime.check() == 1) // check if the metro has passed its interval .
+    {
+      if (value.doorlock == HIGH)
+      {
+        value.doorlock = LOW;
+      }
+      else
+      {
+        value.doorlock = HIGH;
+      }
+      door.writeMicroseconds(1250);
+      value.doorState = true;
+    }
+    else if (pos == 0 and value.doorState == true)
+    { // open door
+      value.doorState = false;
+    }
   }
-  else if(pos == 0 and doorState == true){ // open door
-    doorState = false;
-
-
-
-
-  }
-
-
-
-
-
-
-
 }
 
+void direction(int dir, int speed)
+{
 
+  if (dir > 0 && value.lastState == 0) // this part takes care of the dirrection change of the motor.
+  {
+    Stop();
 
+    if (value.motorOut != 1500)
+    {
+      return;
+    }
+    value.lastState = 1;
+  }
+  else if (dir < 0 && value.lastState == 1)
+  {
+    Stop();
 
+    if (value.motorOut != 1500)
+    {
+      return;
+    }
+    value.lastState = 0;
+  }
+  else
+  {
+    int my_dir;
+    int newspeed;
 
+    if (dir > 0)
+    {
+      newspeed = map(speed, 0, 255, 1500, 2000);
+      my_dir = 5;
+    }
+    else if (dir < 0)
+    {
+      newspeed = map(speed, 0, 255, 1500, 1000);
+      my_dir = -5;
+    }
 
+    if (value.ramp_up.check() == 1) // check if the metro has passed its interval .
+    {
+      if (value.ramp_state == HIGH)
+      {
+        value.ramp_state = LOW;
+      }
+      else
+      {
+        value.stopState = HIGH;
+      }
+
+      if (value.motorOut != newspeed)
+      {
+        value.motorOut += my_dir;
+      }
+      motor.write(value.motorOut);
+    }
+  }
+}
+
+void print_data()
+{
+
+  if (print_val.check() == 1) // check if the metro has passed its interval .
+  {
+    if (print_state == HIGH)
+    {
+      print_state = LOW;
+    }
+    else
+    {
+      print_state = HIGH;
+    }
+    Serial.print("MotorOut");
+    Serial.print("  ");
+    Serial.println(value.motorOut);
+  }
+}
 
 void setup()
 {
@@ -134,28 +226,30 @@ void setup()
   myPID.SetOutputLimits(0, 500);
   myPID.SetMode(AUTOMATIC);
   Serial.begin(115200);
-  
+  value.motorOut = 2000;
 }
 
 void loop()
 {
-  
-  switch (metrostate)
+
+  switch (state)
   {
   case STOP:
-    Stop();
-    Serial.println("STOP");
+    direction(1, 1750);
+    print_data();
+    // Stop();
+    // Serial.println("STOP");
     break;
   case START:
-  Serial.println("START");
+    Serial.println("START");
     // Start();
     break;
   case EAST:
-    // do something
+    direction(1, 200); // 1 to move EAST and 200 is the set speed
     Serial.println("EAST");
     break;
   case WEST:
-    //do something
+    direction(-1, 200); //-1 to move WEST and 200 is the set speed
     Serial.println("WEST");
     break;
   case OPEN:
@@ -168,16 +262,15 @@ void loop()
     break;
   case EMERGENCY:
     // do something
+    motor.writeMicroseconds(1500);
     Serial.println("EMERGENCY");
     break;
   }
 
   long newPosition = myEnc.read();
-  if (newPosition != oldPosition)
+  if (newPosition != value.oldPosition)
   {
-    oldPosition = newPosition;
+    value.oldPosition = newPosition;
     Input = newPosition;
   }
- 
 }
-
